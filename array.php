f4d514ff63c5367abd0b3b57e76170c764e98916
Muhammad Sumon Molla Selim <me@sumonselim.com>
2014-11-23 09:49:30 +0600

    code cleaned

--- array.php
+++ array.php
@@ -5,11 +5,11 @@ $my_array = array(1, 4, 6);
 //In PHP 5.4+:
 $my_array = [1, 4, 6];
 
-
 // accessing a value
 echo $my_array[0] . "<br/>"; // first value. index 0
 
-//// print the array
+// print the array
 print_r($my_array);
 echo "<br/>";
- 
+
+
